ba80a2d833ddd6dcc68e3c72886fc2fb444b14b0
PrakVisal <visalprak2015@gmail.com>
2025-02-14 13:25:59 +0700

    no css

--- script.js
+++ script.js
@@ -5,29 +5,62 @@ const item1 = document.getElementById("item1")
 const item2 = document.getElementById("item2")
 const item3 = document.getElementById("item3")
 const quotes = [
-    { id: "1", title: "Know what it will look like at the TOP", quote: "It's okay if you don't know what it will look like at the TOP. Take it ONE STEP at a time." },
-    { id: "2", title: "Take it one STEP at a time", quote: "It's okay if you don't know what it will look like at the TOP. Take it ONE STEP at a time." },
-    // ... more quotes
+    { id: "1", author: "Albert Einstein", quote: "“Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.”" },
+    { id: "2", author: "Bernard M. Baruch", quote: "“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”" },
+    { id: "3", author: "Mahatma Gandhi", quote: "“Be the change that you wish to see in the world.”"}
 ];
 function result (input){
     try {
         if(select.value=="id"){
-            if(input==1){
-                
+            if(input==1 || input == "albert"){
                 item1.style.display = "block"
+                quote1.innerHTML = quotes[0].quote
+                author1.innerHTML = quotes[0].author
                 item2.style.display = "none"
                 item3.style.display = "none"
-            }else if(input == 2){
-                item1.style.display = "none"
+            }else if(input == 2 ){
                 item2.style.display = "block"
+                quote2.innerHTML = quotes[1].quote
+                author2.innerHTML = quotes[1].author
+                item1.style.display = "none"
                 item3.style.display = "none"
-            }else if(input == 3){
+            }else if(input == 3 ){
+                item3.style.display = "block"
+                quote3.innerHTML = quotes[2].quote
+                author3.innerHTML = quotes[2].author
                 item1.style.display = "none"
                 item2.style.display = "none"
-                item3.style.display = "block"
+            }else{
+                alert("Invalid ID")
+                item1.style.display = "none"
+                item2.style.display = "none"
+                item3.style.display = "none"
             }
-        }else if(select.value == "title"){
-            
+        }else if(select.value == "author"){
+            if(input == "albert"){
+                item1.style.display = "block"
+                quote1.innerHTML = quotes[0].quote
+                author1.innerHTML = quotes[0].author
+                item2.style.display = "none"
+                item3.style.display = "none"
+            }else if(input == "bernard"){
+                item2.style.display = "block"
+                quote2.innerHTML = quotes[1].quote
+                author2.innerHTML = quotes[1].author
+                item1.style.display = "none"
+                item3.style.display = "none"
+            }else if(input == "mahatma"){
+                item3.style.display = "block"
+                quote3.innerHTML = quotes[2].quote
+                author3.innerHTML = quotes[2].author
+                item1.style.display = "none"
+                item2.style.display = "none"
+            }else{
+                alert("Not Found!!!!!")
+                item1.style.display = "none"
+                item2.style.display = "none"
+                item3.style.display = "none"
+            }
         }else if(select.value == "default"){
             throw(error);
         }
